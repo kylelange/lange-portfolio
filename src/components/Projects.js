449e58f77aa23302fa154ac183a44f3a647a8c85
Kyle Lange <baronsintrees@gmail.com>
2017-03-21 21:36:56 -0700

    add html structure to first project tile.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,8 +16,15 @@ class Projects extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1 className="pro11">Hello World!</h1>
+      <div className="project-flex">
+        <div className="text-box">
+          <h2>Lumen Learning: TEACHER TOOLS</h2>
+          <a src='#' ><h3>Case Study</h3></a>
+          <a src='#' ><h3>See the code</h3></a>
+          <p>Lumen Learning makes open source college coursework to help students learn more effectively.  I made a settings menu that made it easier for instructors to reach their students.</p>
+          <h6></h6>
+        </div>
+        <img src="images/settings-window.jpg" alt="a settings page" className="screenshot"></img>
       </div>
     );
   }
