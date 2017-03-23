aefc435f67e6359cba0bcec96bf1884cae98da15
Kyle Lange <baronsintrees@gmail.com>
2017-03-23 16:01:23 -0700

    add media queries for project tile.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,10 +19,11 @@ class Projects extends React.Component {
       <div className="project-flex">
         <div className="text-box">
           <h2>Lumen Learning: TEACHER TOOLS</h2>
+          <img src="images/settings-window.jpg" alt="a settings page" className="screenshot-mobile-appear"></img>
           <div className="flex-default">
             <a href='https://github.com/kylelange/teacher-tools' ><h3>CASE STUDY</h3></a>
             <h3>|</h3>
-            <a src='#' ><h3>SEE THE CODE</h3></a>
+            <a href='https://github.com/kylelange/teacher-tools' ><h3>SEE THE CODE</h3></a>
           </div>
           <p>Lumen Learning makes open source college coursework to help students learn more effectively.  I made a settings menu that made it easier for instructors to reach their students.</p>
           <h6>React.js, CSS3, PostgreSQL, Sketch3, HTML5, UX Research</h6>
