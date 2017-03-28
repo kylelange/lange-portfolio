efa3f92cb6a56d271a1761b9b54737d829f0a5e8
Kyle Lange <baronsintrees@gmail.com>
2017-03-28 09:44:27 -0700

    add second full projects tile.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,19 +16,36 @@ class Projects extends React.Component {
 
   render() {
     return (
-      <div className="project-flex">
-        <div className="text-box">
-          <h2>Lumen Learning: TEACHER TOOLS</h2>
-          <img src="images/settings-window.jpg" alt="a settings page" className="screenshot-mobile-appear"></img>
-          <div className="flex-default">
-            <a href='https://github.com/kylelange/teacher-tools' ><h3>CASE STUDY</h3></a>
-            <h3>|</h3>
-            <a href='https://github.com/kylelange/teacher-tools' ><h3>SEE THE CODE</h3></a>
+      <div>
+        <div className="project-flex">
+          <div className="text-box">
+            <h2>Lumen Learning: TEACHER TOOLS</h2>
+            <img src="images/settings-window.jpg" alt="a settings page" className="screenshot-mobile-appear"></img>
+            <div className="flex-default">
+              <a href='https://github.com/kylelange/teacher-tools' ><h3>CASE STUDY</h3></a>
+              <h3>|</h3>
+              <a href='https://github.com/kylelange/teacher-tools' ><h3>SEE THE CODE</h3></a>
+            </div>
+            <p>Lumen Learning makes open source college coursework to help students learn more effectively.  I made a settings menu that made it easier for instructors to reach their students.</p>
+            <h6>React.js, CSS3, PostgreSQL, Sketch3, HTML5, UX Research</h6>
           </div>
-          <p>Lumen Learning makes open source college coursework to help students learn more effectively.  I made a settings menu that made it easier for instructors to reach their students.</p>
-          <h6>React.js, CSS3, PostgreSQL, Sketch3, HTML5, UX Research</h6>
+          <img src="images/settings-window.jpg" alt="a settings page" className="screenshot"></img>
         </div>
-        <img src="images/settings-window.jpg" alt="a settings page" className="screenshot"></img>
+
+        <div className="project-flex">
+          <img src="images/physio-splash.jpg" alt="the splash photo of a physical therapy website. In it, two people are holding hands." className="screenshot"></img>
+          <div className="text-box">
+            <h2>PhysioPDX: WEBSITE Design/ Implementation</h2>
+            <img src="images/physio-splash.jpg" alt="the splash photo of a physical therapy website. In it, two people are holding hands." className="screenshot-mobile-appear"></img>
+            <div className="flex-default">
+              <a href='https://github.com/kylelange/physio-pdx' ><h3>CASE STUDY</h3></a>
+              <h3>|</h3>
+              <a href='https://github.com/kylelange/physio-pdx' ><h3>SEE THE CODE</h3></a>
+            </div>
+            <p>PhysioPDX is a brick and mortar small business focused on physical healing. They needed a simple site to start a conversation with prosepctive clients and begin to answer the questions they have about their care.</p>
+            <h6>HTML5, CSS3, JQuery/JavaScript, Sketch3, UI design</h6>
+          </div>
+        </div>
       </div>
     );
   }
