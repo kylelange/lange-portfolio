39b37575a4df7fb5a6f226042de7706a26fae5eb
Kyle Lange <baronsintrees@gmail.com>
2017-04-03 22:45:08 -0700

    add initial structure and styles for aboutme component.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -16,8 +16,19 @@ class AppComponent extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1 className="test-h1">Hello World!</h1>
+      <div className="aboutme">
+        <div className="aboutme-col1">
+          <h1 className="aboutme-title">ABOUT ME:</h1>
+          <p>To my core, I am a person who seeks ways to guide others and myself towards growth in learning.  I love web development because I enjoy taking big ideas and finding a way to symbolize them visually, interactively, and creatively. I also am passionate about open-source work and its effect for social justice and educational opportunities.</p>
+          <h6>Code: React.js, HTML5, CSS3 w/Sass, vanilla ES6 JavaScript, jQuery, and a dabbler in Angular JS and TypeScript.  Willing to learn any and all languages (I learned te basics of React.js in three weeks.) </h6>
+        </div>
+        <div className="aboutme-col2">
+          <h2 className="aboutme-title">CONTACT ME:</h2>
+          <p class className="aboutme-email"><a href="mailto:baronsintrees@gmail.com"> baronsintrees@gmail.com</a></p>
+          <img src="images/github-logo.png" alt="a logo of my github repositories" className="aboutme-logo"></img>
+          <img src="images/linkedin-logo.png" alt="a logo of m linkedIn account" className="aboutme-logo"></img>
+          <img src="images/resume-logo.png" alt="a logo of my design: representing a " className="aboutme-logo"></img>
+        </div>
       </div>
     );
   }
