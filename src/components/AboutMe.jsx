9d78532db420b30f71e22db1b026b8e5829fc37a
Kyle Lange <baronsintrees@gmail.com>
2017-04-04 16:39:08 -0700

    add styles to about me text and links to all media online elsewhere.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -19,15 +19,16 @@ class AppComponent extends React.Component {
       <div className="aboutme">
         <div className="aboutme-col1">
           <h1 className="aboutme-title">ABOUT ME:</h1>
-          <p>To my core, I am a person who seeks ways to guide others and myself towards growth in learning.  I love web development because I enjoy taking big ideas and finding a way to symbolize them visually, interactively, and creatively. I also am passionate about open-source work and its effect for social justice and educational opportunities.</p>
-          <h6>Code: React.js, HTML5, CSS3 w/Sass, vanilla ES6 JavaScript, jQuery, and a dabbler in Angular JS and TypeScript.  Willing to learn any and all languages (I learned te basics of React.js in three weeks.) </h6>
+          <p className="aboutme-p">To my core, I am a person who seeks ways to guide others and myself towards growth in learning.  I love web development because I enjoy taking big ideas and finding a way to symbolize them visually, interactively, and creatively. I also am passionate about open-source work and its effect for social justice and educational opportunities.</p>
+          <h6 className="aboutme-h6">Code: React.js, HTML5, CSS3 w/Sass, vanilla ES6 JavaScript, jQuery, and a dabbler in Angular JS and TypeScript.  Willing to learn any and all languages (I learned te basics of React.js in three weeks.) </h6>
         </div>
         <div className="aboutme-col2">
           <h2 className="aboutme-title">CONTACT ME:</h2>
           <p class className="aboutme-email"><a href="mailto:baronsintrees@gmail.com"> baronsintrees@gmail.com</a></p>
-          <img src="images/github-logo.png" alt="a logo of my github repositories" className="aboutme-logo"></img>
-          <img src="images/linkedin-logo.png" alt="a logo of m linkedIn account" className="aboutme-logo"></img>
-          <img src="images/resume-logo.png" alt="a logo of my design: representing a " className="aboutme-logo"></img>
+          <a href="https://github.com/kylelange" ><img src="images/github-logo.png" alt="a logo of my github repositories" className="aboutme-logo"></img></a>
+          <a href="https://www.linkedin.com/in/kyle-lange-dev/" ><img src="images/linkedin-logo.png" alt="a logo of my linkedIn account" className="aboutme-logo"></img></a>
+        {/*href link not working*/}
+          <a href='public/Lange_Resume_Apr17.pdf'><img src="images/resume-logo.png" alt="a logo of my design: representing a resume" className="aboutme-logo"></img></a>
         </div>
       </div>
     );
